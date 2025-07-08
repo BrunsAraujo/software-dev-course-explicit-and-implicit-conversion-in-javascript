909521dc3dd73dee6926db8ac537ef9297242068
Bruno Araujogit <bruno.correadearaujo@gmail.com>
2025-07-08 13:42:27 -0500

    all changes made

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -1,5 +1,3 @@
-/*
-
 Part 1: Debugging Challenge
 The JavaScript code below contains intentional bugs related to type conversion.
 Please do the following:
@@ -18,7 +16,6 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 */
 
-
 let result = "5" - 2;
 console.log("The result is: " + result);
 
@@ -30,3 +27,78 @@ if (isValid) {
 let age = "25";
 let totalAge = age + 5;
 console.log("Total Age: " + totalAge);
+
+TASK 1 ANSWER:
+ORIGINAL CODE:
+let result = "5" - 2;
+console.log("The result is: " + result);
+
+let isValid = Boolean("false");
+if (isValid) {
+    console.log("This is valid!");
+}
+
+let age = "25";
+let totalAge = age + 5;
+console.log("Total Age: " + totalAge);
+
+ERRORS DISPLAYED WHEN CODE RAN:
+The result is: 3
+This is valid!
+Total Age: 255
+CHANGES MADE TO CODE
+let age = 25; removed “ “
+let totalAge = (`age` + 5); - added () and `` - the code here works because of implicit conversion. Javascript is getting “age” a string transforming into a number and adding to 5
+console.log(age + totalAge); - here age 25 is added to total age 30 and the output is 55
+
+RESPONSE WHEN CODE RAN
+$ node explicit-and-implicit-conversion-in-javascript.js
+The result is: 3
+This is valid!
+55
+
+TASK 2 ANSWER:
+IMPLICIT TYPE CONVERSION
+let waiterRobot = `Hello`;
+
+console.log(waiterRobot)
+
+let iswaiterRobotServing = Boolean(waiterRobot);
+
+console.log(iswaiterRobotServing);
+
+let waiterRobotResting = null;
+
+console.log(waiterRobotResting);
+
+let iswaiterRobotResting = Boolean(waiterRobotResting)
+
+console.log(iswaiterRobotResting)	
+
+$ node main.js
+Hello
+true
+null
+false
+
+EXPLICIT TYPE CONVERSION
+
+let redCars = `36`;
+let actualRedCars = Number(redCars);
+
+console.log(typeof redCars);
+console.log(typeof actualRedCars);
+
+let actualRedCarsCrashed =`lemons`;
+
+console.log(actualRedCarsCrashed);
+
+let manyActualRedCarsCrashed = Boolean(actualRedCarsCrashed);
+
+console.log(manyActualRedCarsCrashed)
+
+$ node main.js
+string
+number
+lemons
+true
